0f70df547bfd85a0fe3b162f194cbf16744ed129
Orisabiyi <orisabiyidavid@gmail.com>
2024-05-27 19:13:30 +0100

    added a script.js and included the leaflet library into the script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+"use strict";
+console.log(true);
+
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    const { latitude, longitude } = position.coords;
+    console.log(latitude, longitude);
+
+    // This is for including and setting the map to a coordinate
+    let map = L.map("map", {
+      center: [latitude, longitude],
+      zoom: 13,
+    });
+
+    // This allows the display of tiles on the map
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      maxZoom: 19,
+      attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }).addTo(map);
+
+    L.marker([latitude, longitude]).addTo(map);
+  },
+
+  function (error) {
+    console.error(error);
+  }
+);
+
+console.log(navigator.geolocation);
