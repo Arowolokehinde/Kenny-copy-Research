668258f55a27357c45f067715ff742f547fcd019
Orisabiyi <orisabiyidavid@gmail.com>
2024-06-03 19:10:52 +0100

    added an overlay, get user current location

--- script.js
+++ script.js
@@ -17,9 +17,14 @@ const headerBookTrainButton = document.querySelector(
   ".header__book-train button"
 );
 
+const headerChooseClass = document.querySelector(
+  ".header__choose-train button"
+);
+
 // get user current position
 const getCurPosition = function (position) {
   const { latitude, longitude } = position.coords;
+  console.log(latitude, longitude);
 
   // initializing the map
   let map = L.map("map", {
@@ -62,3 +67,36 @@ trainClassList.addEventListener("click", function (e) {
   e.target.closest(".header__choose-item").classList.toggle("active");
   console.log(e.target.closest(".header__choose-item"));
 });
+
+// get user location
+const getAddress = async function (latitude, longitude) {
+  try {
+    const data = await fetch(
+      `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}`
+    );
+    const res = await data.json();
+
+    currentLocationInput.value = res.display_name;
+
+    currentLocationInput.disabled = true;
+
+    console.log(res.display_name);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+headerChooseClass?.addEventListener("click", function () {
+  document.querySelector(".overlay").classList.toggle("hidden");
+  console.log(document.querySelector(".overlay"));
+});
+
+document.querySelector(".overlay")?.addEventListener("click", function (e) {
+  console.log(e.target.classList.value);
+  if (e.target.classList.value === "overlay")
+    document.querySelector(".overlay").classList.toggle("hidden");
+});
+
+document.addEventListener("DOMContentLoaded", () =>
+  getAddress(6.5018255, 3.369134)
+);
